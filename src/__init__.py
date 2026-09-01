3642b92d209bc5bd82a246a1c61667d64251a06d
roberio25 <roberiofc_@hotmail.com>
2026-08-31 22:08:56 -0300

    chore: adicionar __init__.py do pacote src

---
+++ src/__init__.py
@@ -0,0 +1,15 @@
+"""
+IPTV Player - Pacote principal da aplicação
+"""
+
+__version__ = "1.0.0"
+__author__ = "Roberto"
+__email__ = "roberio25@github.com"
+__license__ = "MIT"
+
+__all__ = [
+    "core",
+    "ui",
+    "utils",
+    "config",
+]
